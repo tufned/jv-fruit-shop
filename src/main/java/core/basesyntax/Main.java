7a01ff1358ce52a2a5182b5f4536661c76bbd8df
Denys Funderat <funderatdenis@gmail.com>
2025-11-12 20:31:25 +0100

    build fixed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,23 +2,23 @@ package core.basesyntax;
 
 import core.basesyntax.db.ShopStorage;
 import core.basesyntax.model.FruitTransaction;
+import core.basesyntax.service.DataConverter;
 import core.basesyntax.service.OperationStrategy;
-import core.basesyntax.service.impl.OperationStrategyImpl;
+import core.basesyntax.service.ReportGenerator;
 import core.basesyntax.service.ShopService;
+import core.basesyntax.service.file.ShopFileReader;
+import core.basesyntax.service.file.ShopFileWriter;
+import core.basesyntax.service.file.impl.ShopFileReaderImpl;
+import core.basesyntax.service.file.impl.ShopFileWriterImpl;
+import core.basesyntax.service.impl.DataConverterImpl;
+import core.basesyntax.service.impl.OperationStrategyImpl;
+import core.basesyntax.service.impl.ReportGeneratorImpl;
 import core.basesyntax.service.impl.ShopServiceImpl;
 import core.basesyntax.strategy.OperationHandler;
 import core.basesyntax.strategy.impl.BalanceOperation;
 import core.basesyntax.strategy.impl.PurchaseOperation;
 import core.basesyntax.strategy.impl.ReturnOperation;
 import core.basesyntax.strategy.impl.SupplyOperation;
-import core.basesyntax.service.DataConverter;
-import core.basesyntax.service.impl.DataConverterImpl;
-import core.basesyntax.service.ReportGenerator;
-import core.basesyntax.service.impl.ReportGeneratorImpl;
-import core.basesyntax.service.file.ShopFileReader;
-import core.basesyntax.service.file.impl.ShopFileReaderImpl;
-import core.basesyntax.service.file.ShopFileWriter;
-import core.basesyntax.service.file.impl.ShopFileWriterImpl;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,17 +32,17 @@ public class Main {
         List<String> inputReport =
                 fileReader.readCsv("src/main/resources/reportToRead.csv");
 
-        // 2. Convert the incoming data into FruitTransactions list
-        DataConverter dataConverter = new DataConverterImpl();
-        List<FruitTransaction> transactions = dataConverter.convertToTransactions(inputReport);
-
-        // 3. Create and feel the map with all OperationHandler implementations
+        // 2. Create and feel the map with all OperationHandler implementations
         Map<FruitTransaction.Operation, OperationHandler> operationHandlers = new HashMap<>();
         operationHandlers.put(FruitTransaction.Operation.BALANCE, new BalanceOperation());
         operationHandlers.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperation());
         operationHandlers.put(FruitTransaction.Operation.RETURN, new ReturnOperation());
         operationHandlers.put(FruitTransaction.Operation.SUPPLY, new SupplyOperation());
         OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlers);
+
+        // 3. Convert the incoming data into FruitTransactions list
+        DataConverter dataConverter = new DataConverterImpl();
+        List<FruitTransaction> transactions = dataConverter.convertToTransactions(inputReport);
 
         // 4. Process the incoming transactions with applicable OperationHandler implementations
         ShopService shopService = new ShopServiceImpl(operationStrategy, storage);
